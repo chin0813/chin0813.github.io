353b1d9a9d4b72ec7ff84bca5ccde2df13641e71
chin0813 <56920830+chin0813@users.noreply.github.com>
2020-12-21 14:55:20 +0800

    first commit

---
+++ build/precache-manifest.c41db49d618fa5113c0f97a2fc32eb91.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92dffae8651a55820d921c61429ca5a2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16d104593449c4714cdf",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "b4926bda896c4dc6b683",
+    "url": "/static/css/main.e67d86e2.chunk.css"
+  },
+  {
+    "revision": "16d104593449c4714cdf",
+    "url": "/static/js/2.17823036.chunk.js"
+  },
+  {
+    "revision": "3eccfdf915b3c38890c5bf6c4194108d",
+    "url": "/static/js/2.17823036.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4926bda896c4dc6b683",
+    "url": "/static/js/main.fc0b0539.chunk.js"
+  },
+  {
+    "revision": "4567734cd469a9475f6c",
+    "url": "/static/js/runtime-main.736723a6.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
